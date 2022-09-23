39886189efdba5394e8bdad8fe4065a688fd4d36
Harasees Singh <76965050+harasees-singh@users.noreply.github.com>
2022-09-24 00:02:19 +0530

    css modules working after adding declarations.d.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import React from "react";
-function App() {
+import AuthForm from "./components/Auth/AuthForm";
+const App: React.FC = () => {
     return (
         <div className="App">
             <header className="App-header">
-                
+                <AuthForm />
             </header>
         </div>
     );
 }
-
 export default App;
